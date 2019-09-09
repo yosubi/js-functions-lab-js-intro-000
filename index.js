1b34cd4f1a01605bc92bf503316d3acb6fc2847f
Joseph Moon <nakkta@gmail.com>
2019-09-09 19:55:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function happyHolidaysTo(name){
 }
 
 function happyHolidayTo(holiday, name) {
-  `Happy ${holiday}, ${name}!`
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday, days) {
-  `It's ${days} days until ${holiday}!`
+  return `It's ${days} days until ${holiday}!`
 }
